865253c6db3b673bee2f9bcdeb9273982bc0f383
Tran Van Khuyen <81480245+hoshikira18@users.noreply.github.com>
2024-10-12 05:46:38 +0700

    done

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -1,6 +1,6 @@
+export { default as Message } from "./message";
 export { default as InputPost } from "./input-post";
+export { default as BackToTop } from "./back-to-top";
 export { default as StarSky } from "./star-background";
 export { default as SignInButton } from "./signin-button";
 export { default as ListMessages } from "./list-messages";
-export { default as Message } from "./message";
-export { default as BackToTop } from "./back-to-top";
