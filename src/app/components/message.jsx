eb96c7d8fcd07e725cf806900e341cb6c5f6c96f
Tran Van Khuyen <81480245+hoshikira18@users.noreply.github.com>
2024-10-12 17:28:01 +0700

    feat: display private messsage

--- src/app/components/message.jsx
+++ src/app/components/message.jsx
@@ -1,25 +1,36 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 
 const Message = ({ data, session }) => {
   const date = new Date(data.created_at);
   const d = date.getDate();
   const m = date.getMonth() + 1;
   const y = date.getFullYear();
+  const [isDisplayPrivate, setIsDisplayPrivate] = useState(false);
 
   console.log(session);
+  const email = "ptkl2053@gmail.com";
+
+  useEffect(() => {
+    if (session.user.email === email) {
+      setIsDisplayPrivate(true);
+    }
+  }, []);
 
   if (data.message_type === "private") {
-    if (
-      session.user.email !== "ptkl2053@gmail.com" ||
-      session.user.email !== "linhptkha170019@fpt.edu.vn"
-    ) {
+    if (session.user.email !== email) {
       return;
     }
   }
-
   return (
     <div className=" grid gap-2 grid-cols-12">
-      <div className=" col-span-2 w-full md:col-span-1 rounded-full aspect-square overflow-hidden border-2 border-green-600">
+      <div
+        className={` col-span-2 w-full md:col-span-1 rounded-full aspect-square overflow-hidden border-2 ${
+          isDisplayPrivate && data.message_type === "private"
+            ? "border-red-600 border-4"
+            : "border-green-600"
+        }`}
+      >
         <img
           src={data.image}
           alt="user_image"
